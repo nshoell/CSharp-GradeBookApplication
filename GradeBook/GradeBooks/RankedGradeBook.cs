332ef5f587d75df4a0d1f6497a87ecd5c75cd8bb
Nathan Shoell <SNathan@ultradent.com>
2019-03-25 13:50:49 -0600

    completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,11 +21,11 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[(threshold * 2) - 1] < +averageGrade)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[(threshold * 3) - 1] < +averageGrade)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[(threshold * 4) - 1] < +averageGrade)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
